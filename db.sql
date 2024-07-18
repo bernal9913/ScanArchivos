924f2d5d3ee74b87f447167a85e17850c4e6214b
Carlos Bernal <carlitos9913@gmail.com>
2024-07-18 11:15:02 -0700

    chambeale mas racing juan

--- db.sql
+++ db.sql
@@ -1,29 +1,76 @@
-CREATE DATABASE IF NOT EXISTS digitalizacion_archivos;
+create table archivos
+(
+    id                    int auto_increment
+        primary key,
+    asunto                varchar(255)                          not null,
+    numero_oficio         varchar(50)                           not null,
+    fecha_creacion        date                                  not null,
+    fecha_subida          timestamp default current_timestamp() null,
+    unidad_administrativa varchar(100)                          not null,
+    folio_inicial         int                                   not null,
+    folio_final           int                                   not null,
+    emisor                varchar(100)                          not null,
+    remitente             varchar(100)                          not null,
+    resumen               text                                  not null,
+    categoria_id          int                                   null,
+    subcategoria_id       int                                   null
+)
+    engine = InnoDB;
 
-USE digitalizacion_archivos;
+create table categorias
+(
+    id     int auto_increment
+        primary key,
+    nombre varchar(255) not null
+)
+    engine = InnoDB;
 
-CREATE TABLE archivos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    asunto VARCHAR(255) NOT NULL,
-    numero_oficio VARCHAR(50) NOT NULL,
-    fecha_creacion DATE NOT NULL,
-    fecha_subida TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    unidad_administrativa VARCHAR(100) NOT NULL,
-    folio_inicial INT NOT NULL,
-    folio_final INT NOT NULL,
-    emisor VARCHAR(100) NOT NULL,
-    remitente VARCHAR(100) NOT NULL,
-    resumen TEXT NOT NULL
-);
+create table subcategorias
+(
+    id           int auto_increment
+        primary key,
+    nombre       varchar(255) null,
+    categoria_id int          null,
+    constraint subcategorias_ibfk_1
+        foreign key (categoria_id) references categorias (id)
+)
+    engine = InnoDB;
 
-select * from archivos;
+create index categoria_id
+    on subcategorias (categoria_id);
 
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    numero_empleado INT UNIQUE NOT NULL,
-    rol VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
+create table usuarios
+(
+    id              int auto_increment
+        primary key,
+    nombre          varchar(100) not null,
+    numero_empleado int          not null,
+    rol             varchar(50)  not null,
+    password        varchar(255) not null,
+    constraint numero_empleado
+        unique (numero_empleado)
+)
+    engine = InnoDB;
 
-select * from usuarios;
+create table historial_versiones
+(
+    id           int auto_increment
+        primary key,
+    documento_id int                                   null,
+    accion       varchar(50)                           null,
+    fecha        timestamp default current_timestamp() null,
+    usuario_id   int                                   null,
+    detalles     text                                  null,
+    constraint historial_versiones_ibfk_1
+        foreign key (documento_id) references archivos (id),
+    constraint historial_versiones_ibfk_2
+        foreign key (usuario_id) references usuarios (id)
+)
+    engine = InnoDB;
+
+create index documento_id
+    on historial_versiones (documento_id);
+
+create index usuario_id
+    on historial_versiones (usuario_id);
+
