aeb40ec794c4f9ec0eb619462cf86c9074a53ba5
Carlos Bernal <carlitos9913@gmail.com>
2024-08-08 12:25:51 -0700

    chambeale mas racing juan

--- app.py
+++ app.py
@@ -82,10 +82,11 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
+
 @app.route('/contacto', methods=['GET'])
 def contacto():
-    if request.method ==  "GET":
-        return render_template("contacto.html")
+    return render_template("contacto.html")
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -119,11 +120,9 @@ def index():
     cursor = db.cursor()
     cursor.execute("SELECT id, nombre FROM categorias")
     cat = cursor.fetchall()
-    print(cat)
 
     cursor.execute("SELECT id, nombre FROM subcategorias")
     subcat = cursor.fetchall()
-    print(subcat)
 
     if request.method == 'POST':
         # Obtener los datos del formulario
@@ -145,19 +144,14 @@ def index():
         sql = """INSERT INTO archivos (asunto, numero_oficio, fecha_creacion, fecha_subida, unidad_administrativa, folio_inicial, folio_final, emisor, remitente, resumen, categoria_id, subcategoria_id) 
                  VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
         cursor.execute(sql, (
-        asunto, numero_oficio, fecha_creacion, fecha_subida, unidad_administrativa, folio_inicial, folio_final, emisor,
-        remitente, resumen, categoria_id, subcategoria_id))
+            asunto, numero_oficio, fecha_creacion, fecha_subida, unidad_administrativa, folio_inicial, folio_final, emisor,
+            remitente, resumen, categoria_id, subcategoria_id))
         documento_id = cursor.lastrowid
         db.commit()
 
-        # archivo.save(os.path.join(app.config['UPLOAD_FOLDER'], str(documento_id)))
-
-        # Registrar la acción en el historial
-        archivo_id = cursor.lastrowid
-
         if archivo:
             filename = secure_filename(archivo.filename)
-            archivo_path = os.path.join(app.config['UPLOAD_FOLDER'], str(archivo_id))
+            archivo_path = os.path.join(app.config['UPLOAD_FOLDER'], str(documento_id))
             if not os.path.exists(archivo_path):
                 os.makedirs(archivo_path)
             archivo.save(os.path.join(archivo_path, filename))
@@ -168,37 +162,102 @@ def index():
 
     return render_template('index.html', categorias=cat, subcategorias=subcat)
 
+
 @app.route('/dashboard', methods=['GET'])
 @login_required
 def dashboard():
-    search_query = request.args.get('search')
+    #search_query = request.args.get('search')
+    search_asunto = request.args.get('search_asunto')
+    search_num_oficio = request.args.get("search_num_oficio")
+    search_fecha_creacion = request.args.get("search_fecha_creacion")
+    search_fecha_subida = request.args.get("search_fecha_subida")
+    search_unidad_administrativa = request.args.get("search_unidad_administrativa")
+    search_folio_inicial = request.args.get("search_folio_inicial")
+    search_folio_final = request.args.get("search_folio_final")
+    search_emisor = request.args.get("search_emisor")
+    search_remitente = request.args.get("search_remitente")
+    search_resumen = request.args.get("search_resumen")
+    search_categoria = request.args.get("search_categoria")
+    search_subcategoria = request.args.get("search_subcategoria")
+
     cursor = db.cursor()
+    sql = """
+            SELECT archivos.id, asunto, numero_oficio, fecha_creacion, fecha_subida, unidad_administrativa, folio_inicial, folio_final, emisor, remitente, resumen, categorias.nombre, subcategorias.nombre 
+            FROM archivos 
+            INNER JOIN categorias on archivos.categoria_id = categorias.id
+            INNER JOIN subcategorias on archivos.subcategoria_id = subcategorias.id
+            WHERE 1=1
+        """
+    params = []
 
-    if search_query:
-        sql = """SELECT archivos.id, asunto, numero_oficio, fecha_creacion, fecha_subida, unidad_administrativa, folio_inicial, folio_final, emisor, remitente, resumen, categorias.nombre, subcategorias.nombre
-                 FROM archivos 
-                 INNER JOIN categorias on archivos.categoria_id = categorias.id
-                 INNER JOIN subcategorias on archivos.subcategoria_id = subcategorias.id
-                 WHERE asunto LIKE %s OR numero_oficio LIKE %s OR emisor LIKE %s OR remitente LIKE %s
-                 """
+    if search_asunto:
+        sql += " AND asunto LIKE %s"
+        params.append(f"%{search_asunto}%")
+    if search_num_oficio:
+        sql += " AND numero_oficio LIKE %s"
+        params.append(f"%{search_num_oficio}%")
+    if search_fecha_creacion:
+        sql += " AND fecha_creacion LIKE %s"
+        params.append(f"%{search_fecha_creacion}%")
+    if search_fecha_subida:
+        sql += " AND fecha_subida LIKE %s"
+        params.append(f"%{search_fecha_subida}%")
+    if search_unidad_administrativa:
+        sql += " AND unidad_administrativa LIKE %s"
+        params.append(f"%{search_unidad_administrativa}%")
+    if search_emisor:
+        sql += " AND emisor LIKE %s"
+        params.append(f"%{search_emisor}%")
+    if search_remitente:
+        sql += " AND remitente LIKE %s"
+        params.append(f"%{search_remitente}%")
+    if search_categoria:
+        sql += " AND categorias.nombre LIKE %s"
+        params.append(f"%{search_categoria}%")
+    if search_subcategoria:
+        sql += " AND subcategorias.nombre LIKE %s"
+        params.append(f"%{search_subcategoria}%")
+    if search_resumen:
+        sql += " AND resumen LIKE %s"
+        params.append(f"%{search_resumen}%")
+    if search_folio_inicial:
+        sql += " AND folio_inicial LIKE %s"
+        params.append(f"%{search_folio_inicial}%")
+    if search_folio_final:
+        sql += " AND folio_final LIKE %s"
+        params.append(f"%{search_folio_final}%")
 
-        cursor.execute(sql, (
-        '%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%'))
-    else:
-        sql = """SELECT archivos.id, asunto, numero_oficio, fecha_creacion, fecha_subida, unidad_administrativa, folio_inicial, folio_final, emisor, remitente, resumen, categorias.nombre, subcategorias.nombre 
-                 FROM archivos 
-                 INNER JOIN categorias on archivos.categoria_id = categorias.id
-                 INNER JOIN subcategorias on archivos.subcategoria_id = subcategorias.id
-                 """
-        cursor.execute(sql)
 
+    cursor.execute(sql, params)
     archivos = cursor.fetchall()
+
     return render_template('dashboard.html', archivos=archivos)
+
+
+@app.route('/ver_archivo/<int:archivo_id>')
+@login_required
+def ver_archivo(archivo_id):
+    cursor = db.cursor()
+    sql = """SELECT archivos.id, asunto, numero_oficio, fecha_creacion, fecha_subida, unidad_administrativa, folio_inicial, folio_final, emisor, remitente, resumen, categorias.nombre, subcategorias.nombre 
+             FROM archivos 
+             INNER JOIN categorias on archivos.categoria_id = categorias.id
+             INNER JOIN subcategorias on archivos.subcategoria_id = subcategorias.id
+             WHERE archivos.id = %s"""
+    cursor.execute(sql, (archivo_id,))
+    archivo = cursor.fetchone()
+
+    sql = """SELECT accion, fecha, usuarios.nombre, detalles 
+             FROM historial_versiones
+             INNER JOIN usuarios ON usuarios.id = historial_versiones.usuario_id
+             WHERE documento_id = %s"""
+    cursor.execute(sql, (archivo_id,))
+    historial = cursor.fetchall()
+    return render_template('ver_archivo.html', archivo=archivo, historial=historial)
 
 
 @app.route('/archivo/<int:archivo_id>')
 @login_required
-def ver_archivo(archivo_id):
+def ver_archivo_file(archivo_id):
     archivo_path = os.path.join(app.config['UPLOAD_FOLDER'], str(archivo_id))
     if os.path.exists(archivo_path):
         archivos = os.listdir(archivo_path)
@@ -207,6 +266,17 @@ def ver_archivo(archivo_id):
             return send_from_directory(archivo_path, archivos[0])
     flash('Archivo no encontrado', 'danger')
     return redirect(url_for('dashboard'))
+
+
+@app.route('/archivo/<int:archivo_id>/ver', methods=['GET'])
+@login_required
+def visualizar(archivo_id):
+    archivo_path = os.path.join(app.config['UPLOAD_FOLDER'], str(archivo_id))
+    if os.path.exists(archivo_path):
+        archivos = os.listdir(archivo_path)
+        if archivos:
+            registrar_historial(archivo_id, "visualizado", f"Archivo visualizado por usuario ID {current_user.id}")
+            return send_from_directory(archivo_path, archivos[0])
 
 
 @app.route('/archivo/<int:archivo_id>/eliminar', methods=['POST'])
@@ -222,16 +292,17 @@ def eliminar_archivo(archivo_id):
     flash('Archivo eliminado exitosamente')
     return redirect(url_for('dashboard'))
 
+
 @app.route('/historial/<int:archivo_id>', methods=['GET'])
 @login_required
 def historial(archivo_id):
     cursor = db.cursor()
     sql = """SELECT accion, fecha, usuarios.nombre, detalles FROM historial_versiones
-         inner join usuarios on usuarios.id = historial_versiones.usuario_id
-         WHERE documento_id = %s"""
+             INNER JOIN usuarios ON usuarios.id = historial_versiones.usuario_id
+             WHERE documento_id = %s"""
     cursor.execute(sql, (archivo_id,))
-    versiones = cursor.fetchall()
-    return render_template('historial.html', versiones=versiones)
+    historial = cursor.fetchall()
+    return render_template('historial.html', historial=historial)
 
 
 if __name__ == '__main__':
